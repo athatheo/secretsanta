d24d1b8f9bd37bbba8f2fdb96b013423b4045b3a
Athanasios Theocharis <athatheoc@gmail.com>
2022-12-06 15:24:56 +0100

    rand

--- SecretSanta.js
+++ SecretSanta.js
@@ -10,12 +10,15 @@ SecretSanta.prototype.add = function ( name ) {
         this.submitter.push( name );
     }
 };
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
 SecretSanta.prototype.generate = function () {
     seed = 'hello'
     Math.seedrandom(seed);
-    console.log(Math.random()); 
-    console.log(Math.random()); 
+    console.log(getRandomInt(6)); 
+    console.log(getRandomInt(6)); 
 
     var pairings = Object.create( null );
     var candidatePairings = Object.create( null );
@@ -25,44 +28,8 @@ SecretSanta.prototype.generate = function () {
 
         if (flag){
             seed = seed + 1
-            Math.seedrandom(x)
+            Math.seedrandom(seed)
         }
     }
-
-    this.names.forEach( function ( name ) {            
-        var candidates = _.difference( this.names, [ name ] );
-        candidatePairings[ name ] = candidates;
-    }, this );
-
-    var findNextGifter = function () {
-
-        var names = Object.keys( candidatePairings );
-
-        var minCandidateCount = _.min( names.map( function ( name ) { return candidatePairings[ name ].length; } ) );
-        var potentialGifters = names.filter( function ( name ) { return candidatePairings[ name ].length === minCandidateCount; } );
-
-        return _.sample( potentialGifters );
-
-    };
-
-    while ( Object.keys( candidatePairings ).length > 0 ) {
-
-        var name = findNextGifter();
-
-        if ( candidatePairings[ name ].length === 0 )
-            throw new Error('We haven\'t been able to find a match for ' + name + '! Press "Generate" to try again and, if it still doesn\'t work, try removing some exclusions from your rules. Sorry for the inconvenience!');
-
-        var pairing = _.sample( candidatePairings[ name ] );
-        delete candidatePairings[ name ];
-
-        Object.keys( candidatePairings ).forEach( function ( name ) {
-            candidatePairings[ name ] = _.without( candidatePairings[ name ], pairing );
-        } );
-
-        pairings[ name ] = pairing;
-
-    }
-
-    return pairings;
 
 };
