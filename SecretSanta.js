ec9e4e929df2a035f70e5fcc00d1e7394b3d787b
Athanasios Theocharis <athatheoc@gmail.com>
2022-12-06 15:33:41 +0100

    rand

--- SecretSanta.js
+++ SecretSanta.js
@@ -1,6 +1,7 @@
 
 var SecretSanta = function () {
     this.submitter = []
+    this.seed = 'hello'
     this.names = ['Tatinator', 'vostidi', 'protagonistis', 'bobos'];
     this.askers = ['soulta1', 'boston2', 'wattwil3', 'megalexandros4','ichwill5','elcapitano6']
 };
@@ -11,25 +12,21 @@ SecretSanta.prototype.add = function ( name ) {
     }
 };
 function getRandomInt(max) {
+    seed = this.seed
+    Math.seedrandom(seed);
     return Math.floor(Math.random() * max);
   }
 
 SecretSanta.prototype.generate = function () {
-    seed = 'hello'
-    Math.seedrandom(seed);
+    
     console.log(getRandomInt(6)); 
     console.log(getRandomInt(6)); 
 
     var pairings = Object.create( null );
     var candidatePairings = Object.create( null );
 
-    var flag = true;
-    while (flag) {
+    for (const asker in this.askers) {
 
-        if (flag){
-            seed = seed + 1
-            Math.seedrandom(seed)
-        }
     }
 
 };
